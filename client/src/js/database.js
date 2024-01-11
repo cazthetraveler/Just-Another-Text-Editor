19e34952468a07d35e84324a097a449be3a16961
MultiMouse <olmos.luisd@outlook.com>
2024-01-10 19:07:07 -0600

    added logic to getDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,6 +26,15 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+export const getDb = async () => {
+  console.log("GET all from the db.");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log(result);
+  console.error("getDb not implemented");
+};
 
 initdb();
